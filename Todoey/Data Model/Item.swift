e986de5538cba31d101b63a9db9160e82ee0e268
subramani simhachalam <iceapple@subramanis-imac.local>
2018-04-10 08:02:33 +0530

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by subramani simhachalam on 4/9/18.
+//  Copyright © 2018 subramani simhachalam. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
